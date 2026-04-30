961a17ab5acab7d6b8bfa0fcee435d70e826ee20
TienHoi <hoangminhtien365@gmail.com>
2026-04-30 20:06:29 +0700

    Add files via upload
    TimeIGuess

---
+++ Time/Month.java
@@ -0,0 +1,19 @@
+public enum Month {
+    JAN(31),
+    FEB(28),
+    MAR(31),
+    APR(30),
+    MAY(31),
+    JUNE(30),
+    JUL(31),
+    AUG(31),
+    SEP(30),
+    OCT(31),
+    NOV(30),
+    DEC(31);
+
+    final int numberOfDays;
+    Month(int Days){
+        this.numberOfDays=Days;
+    }
+}
