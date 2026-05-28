deae51601c5076ea6ed1858f2055863b821eae37
TruggNah <quoctrungpt2006@gmail.com>
2026-05-28 14:43:31 +0700

    Delete view directory

--- view/Launcher.java
+++
@@ -1,9 +0,0 @@
-package view;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(MapViewer.class, args);
-    }
-}
